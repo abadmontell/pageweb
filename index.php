e7551f561b7c39651e1704e047822482a967a31d
amontiel <abadmontell@gmail.com>
2023-10-16 13:16:28 -0600

    Se agrego el footer

---
+++ index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Calendarios LEN</title>
+    <meta name="description" content="Incremente sus ingresos vendiendo calendarios promocionales por catálogo, esta es una oportunidad de negocio para impresores, diseñadores, vendedores independientes y cualquier persona que desee multiplicar su dinero. No necesita experiencia. Sus clientes podrán promocionar sus productos o servicios todo el año, con un regalo muy útil, de bajo costo, alta calidad, y gran tradición.">
+    <meta name="keywords" content="venta por catálogo, calendario promocional, calendario publicitario, bolsa promocional, calendarios len, oportunidad de negocio, incremente sus ingresos, publicidad, calendarios 2023">
+    <meta http-equiv=”Permissions-Policy” content=”interest-cohort=()”>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+
+
+    <nav class="encabezado1">
+        <div class="container-fluid">
+            <ul class="linkLogo">
+                <li>
+                    <a href=""><img src="images/len50.svg" alt=""></a>
+                </li>
+            </ul>
+            <ul class="links">
+                <li class="">
+                    <a href="">Llame sin costo al 800-062-3440</a>
+                </li>
+                <li class="">
+                    <a href="">FACTURACIÓN</a>
+                </li>
+                <li class="">
+                    <a href="">MI CUENTA</a>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
+    <nav class="encabezado2">
+        <div class="container">
+            <ul>
+                <li style="padding-top: 10px;">
+                    <a href="">Oportunidad de negocio</a>
+                </li>
+                <li style="padding-top: 10px;">
+                    <a href="">Productos LEN</a>
+                </li>
+                <li>
+                    <a href="">El calendario como Promocional</a>
+                </li>
+                <li style="padding-top: 10px;">
+                    <a href="">Preservando Tradiciones</a>
+                </li>
+                <li style="padding-top: 10px;">
+                    <a href="">Quiénes Somos</a>
+                </li>
+                <li style="padding-top: 10px;">
+                    <a href="">Preguntas frecuentes</a>
+                </li>
+                <li style="padding-top: 22px;">
+                    <a href="">Blog</a>
+                </li>
+                <li style="padding-top: 22px;">
+                    <a href="">Contacto</a>
+                </li>
+                <div style="padding-top: 10px;">
+                    <a href="" class="btnLink" style="margin-left: 15px;"><i class="fa fa-whatsapp"></i></a>
+                    <a href="" class="btnLink"><i class="fa fa-facebook-official"></i></a>
+                    <a href="" class="btnLink"><i class="fa fa-instagram"></i></a>
+                    <a href="" class="btnLink"><i class="fa fa-youtube-play"></i></a>
+                </div>
+            </ul>
+        </div>
+    </nav>
+    <div class="seccion" id="banner">
+        <div id="demo" class="carousel slide" data-ride="carousel">
+
+            <!-- Indicators -->
+            <ul class="carousel-indicators">
+                <li data-target="#demo" data-slide-to="0" class="active"></li>
+                <li data-target="#demo" data-slide-to="1"></li>
+                <li data-target="#demo" data-slide-to="2"></li>
+            </ul>
+
+            <!-- The slideshow -->
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="images/banner/bn-bolsas-nuevas.png" alt="Bolsas" width="100%">
+                </div>
+                <div class="carousel-item">
+                    <img src="images/banner/bn-calendarios-lenv2.png" alt="LEN" width="100%">
+                </div>
+                <div class="carousel-item">
+                    <img src="images/banner/bn-calendarios-personalizados.png" alt="Clientes" width="100%">
+                </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+            </a>
+            <a class="carousel-control-next" href="#demo" data-slide="next">
+                <span class="carousel-control-next-icon"></span>
+            </a>
+        </div>
+    </div>
+
+
+    <div class="seccion" id="productos">
+        <div class="tituloAzul">
+            <img src="images/conozca_nuestros_productos.png" alt="">
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3" style="padding-top: 15px;">
+                    <a href=""><img src="images/calendarios len 2023.png" alt=""></a>
+                </div>
+                <div class="col-sm-3" style="padding-top: 110px;">
+                    <a href=""><img src="images/calendarios personalizados.png" alt=""></a>
+                </div>
+                <div class="col-sm-3" style="padding-top: 70px;">
+                    <a href=""><img src="images/bolsa promocional len.png" alt=""></a>
+                </div>
+                <div class="col-sm-3" style="padding-top: 57px;">
+                    <a href=""><img src="images/tortilleros len.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="seccion" id="redesSociales">
+        <div class="tituloSinColor">
+            <img src="images/u799818-4.png" alt="">
+        </div>
+
+        <div class="redesSocialesContenido">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                   
+                    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D100090910343781&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                    </div>
+                    <div class="col-sm-6"><br>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/fFDgF9V5sPU?si=AbO2IIVFmSLzsO6w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="seccion" id="contacto">
+        
+    </div>
+
+    <div class="seccion" id="footer">
+        <div class="container">
+            <div class="row">
+            <div class="col-sm-4">
+                <img src="images/len.svg" alt="logo">
+            </div>
+            <div class="col-sm-4">
+             
+                <p>Contacto</p>
+                <p>800-062-3440</p>
+                <p>E-mail: atencionclientes@len.com.mx</p>
+            </div>
+            <div class="col-sm-4">
+         
+                <p>Redes sociales</p>
+                <p><a href="" class="btnLink" style="margin-left: 15px;"><i class="fa fa-whatsapp"></i></a>
+                    <a href="" class="btnLink"><i class="fa fa-facebook-official"></i></a>
+                    <a href="" class="btnLink"><i class="fa fa-instagram"></i></a>
+                    <a href="" class="btnLink"><i class="fa fa-youtube-play"></i></a></p>
+                
+            </div>
+            <div class="col-sm-12">
+                <br>
+            <p>DERECHOS RESERVADOS® LITO OFFSET LEN, S.A. DE C.V.</p>
+            </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
